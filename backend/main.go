d2ee8513cbdd0a316f2ef71fe1edd8ebe1c46805
Jacob Adams <jacob@tookmund.com>
2020-09-26 18:08:02 -0400

    Log web requests

--- backend/main.go
+++ backend/main.go
@@ -40,5 +40,13 @@ func root(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/send", sendMessage)
 	http.HandleFunc("/", root)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Print("Listening on 8080...\n")
+	log.Fatal(http.ListenAndServe(":8080", logRequest(http.DefaultServeMux)))
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
