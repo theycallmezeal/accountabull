bf993b236188e15a63ef055a7734126560a64f3b
Adam <zealan@gmail.com>
2020-09-26 19:54:10 -0400

    get datetime working!!!!

--- frontend/script.js
+++ frontend/script.js
@@ -14,7 +14,50 @@ const app = new Vue({
 	el: "#app",
 	router: new VueRouter({ routes: routes }),
 	data: {
-		"foo": 5
+		tasks: [{"name": "Do laundry", "time": new Date()}],
+		friends: [],
+		name: [],
+		email: "",
+		facebook: "",
+		twitter: "",
+		linkedin: "",
+		phone: ""
+	},
+	methods: {
+		getDate: function(datetime) {
+			const offset = datetime.getTimezoneOffset()
+			datetime = new Date(datetime.getTime() - (offset*60*1000))
+			return datetime.toISOString().split('T')[0];
+		},
+		setDate: function(task, datetime) {
+			var hours = task.time.getHours();
+			var minutes = task.time.getMinutes();
+			datetime.setHours(hours);
+			datetime.setMinutes(minutes);
+			task.time = datetime;
+		},
+		getTime: function(datetime) {
+			var hours = datetime.getHours() + "";
+			if (hours.length < 2) {
+				hours = "0" + hours;
+			}
+			var minutes = datetime.getMinutes() + "";
+			if (minutes.length < 2) {
+				minutes = "0" + minutes;
+			}
+			return hours + ":" + minutes;
+		},
+		setTime: function(task, datetime) {
+			const offset = datetime.getTimezoneOffset()
+			datetime = new Date(datetime.getTime() + (offset*60*1000))
+			var year = task.time.getFullYear();
+			var month = task.time.getMonth();
+			var day = task.time.getDate();
+			datetime.setYear(year);
+			datetime.setMonth(month);
+			datetime.setDate(day);
+			task.time = datetime;
+		}
 	},
 	computed: {
 		path() {
