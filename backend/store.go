04f66f4e981095b392509ec07a7d2ba0cb2e7249
Jacob Adams <jacob@tookmund.com>
2020-09-27 01:50:31 -0400

    Cross-platform paths

--- backend/store.go
+++ backend/store.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 var storefs http.Handler = http.FileServer(http.Dir("store"))
@@ -13,7 +14,7 @@ func storeData(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		storefs.ServeHTTP(w, r)
 	} else if r.Method == http.MethodPost {
-		f, err := os.Create("store"+r.URL.Path)
+		f, err := os.Create(filepath.FromSlash("store"+r.URL.Path))
 		if err != nil {
 			log.Printf("Store error: %s %s", r.URL.Path, err)
 			return
