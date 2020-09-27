4026bf3027d68f2301e17853d92ec1d80b052be8
Jacob Adams <jacob@tookmund.com>
2020-09-26 23:53:23 -0400

    Basic userdata store

---
+++ backend/store.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"io"
+	"log"
+	"net/http"
+)
+
+var storefs http.Handler = http.FileServer(http.Dir("store"))
+
+func storeData(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		storefs.ServeHTTP(w, r)
+	} else if r.Method == http.MethodPost {
+		f, err := os.Create("store"+r.URL.Path)
+		if err != nil {
+			log.Printf("Store error: %s %s", r.URL.Path, err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, r.Body)
+	}
+}
